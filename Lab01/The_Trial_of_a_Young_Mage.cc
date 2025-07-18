96e41ff6807b2e54c47cd751baeafa3198069694
1lDX <worrapon453@gmail.com>
2025-07-18 17:07:14 +0700

    Finished_young-mage

--- Lab01/The_Trial_of_a_Young_Mage.cc
+++ Lab01/The_Trial_of_a_Young_Mage.cc
@@ -3,20 +3,82 @@
 #include <string>
 using namespace std;
 
-int calScore(vector<int> spell_cost,
-             vector<int> spell_level,
-             vector<char> spell_char,
-             int requirement, int mana){
+
+double myMax(double take,double noTake){
+    if(take > noTake){
+        return take;
+    }else if (noTake > take){
+        return noTake;
+    }
+    return take;
+}
+
+double knapSackRecur(int i, int count,int limit,int capacity, vector<double> val, vector<int> wt) {
+    if (count == limit ||i==val.size()) return 0;
+    
+    double take = 0;
+    if (wt[i]<=capacity) {
+        take = val[i] + knapSackRecur(i, count +1,limit,capacity-wt[i], val, wt);
+    }
+    
+    double noTake = knapSackRecur(i+1, count,limit,capacity, val, wt);
+
+    
+    return myMax(take, noTake);
+}
+
+// int cal(vector<int> spell_cost,
+//              vector<int> spell_level,
+//              vector<char> spell_char,
+//              int magic,int requirement, int mana){
+            
+
+//             for(int i=0;i<magic;i++){
                 
-             }
+//             }
+//     }
+
+
+double calScore(char spell_char,int spell_level){
+    double score = 0.0;
+    double score_level;
+    if (spell_char == 'F') {
+        score = 12;
+    } else if (spell_char == 'A') {
+        score = 15;
+    } else if (spell_char == 'W') {
+        score = 12; 
+    } else if (spell_char == 'E') {
+        score = 10; 
+    } else if (spell_char == 'L') {
+        score = 20;
+    }  else if (spell_char == 'D') {
+        score = 20;
+    }
+
+    if (spell_level >= 1 && spell_level <= 5){
+        score_level = spell_level * 10;
+    }else if (spell_level == 6){
+        score_level = 65;
+    }else if (spell_level == 7){
+        score_level = 80;
+    }else if (spell_level == 8){
+        score_level = 100;
+    }
+
+    double result = (score * score_level)/100;
+    return result;
+}
+
 
 int main() {
     int requirement, mana, magic;
     cin >> requirement >> mana >> magic;
 
-    vector<char> spell_char;
-    vector<int> spell_level;
-    vector<int> spell_cost;
+    vector<char> spell_char(magic);
+    vector<int> spell_level(magic);
+    vector<int> spell_cost(magic);
+    vector<double> score(magic);
 
     for (int i = 0; i < magic; i++) {
         char type;
@@ -27,7 +89,7 @@ int main() {
         cin >> type >> level >> name1;
         cin >> name2;
 
-    
+
         if (isdigit(name2[0])) {
             cost = stoi(name2);
         } else {
@@ -35,10 +97,14 @@ int main() {
             cin >> cost;
         }
 
-        spell_char.push_back(type);
-        spell_level.push_back(level);
-        spell_cost.push_back(cost);
+        score[i] = calScore(type,level);
+        spell_char[i] = type;
+        spell_level[i] = level;
+        spell_cost[i] = cost;
+        
     }
+
+    cout << knapSackRecur(0,0,requirement,mana,score,spell_cost) << endl;
 
     
     // for (int i = 0; i < magic; i++) {
---
+++ Lab01/tempCodeRunnerFile.cc
@@ -0,0 +1,10 @@
+int cal(vector<int> spell_cost,
+//              vector<int> spell_level,
+//              vector<char> spell_char,
+//              int magic,int requirement, int mana){
+            
+
+//             for(int i=0;i<magic;i++){
+                
+//             }
+//     }
